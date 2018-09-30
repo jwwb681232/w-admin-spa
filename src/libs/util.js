4c0b2dcb846a520f42ac03a6f92bb94b66af18fb
wangxiao <wx497657341@qq.com>
2018-09-30 11:08:44 +0800

    format

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,51 +1,50 @@
 import Cookies from 'js-cookie'
 // cookie保存的天数
 import config from '@/config'
-import { forEach, hasOneOf } from '@/libs/tools'
+import {forEach, hasOneOf} from '@/libs/tools'
 
 export const TOKEN_KEY = 'token'
 
 export const setToken = (token) => {
-  Cookies.set(TOKEN_KEY, token, {expires: config.cookieExpires || 1})
+    Cookies.set(TOKEN_KEY, token, {expires: config.cookieExpires || 1})
 }
 
 export const getToken = () => {
-  const token = Cookies.get(TOKEN_KEY)
-  if (token) return token
-  else return false
+    const token = Cookies.get(TOKEN_KEY)
+    if (token) return token
+    else return false
 }
 
 export const hasChild = (item) => {
-  return item.children && item.children.length !== 0
+    return item.children && item.children.length !== 0
 }
 
 const showThisMenuEle = (item, access) => {
-  if (item.meta && item.meta.access && item.meta.access.length) {
-    if (hasOneOf(item.meta.access, access)) return true
-    else return false
-  } else return true
+    if (item.meta && item.meta.access && item.meta.access.length) {
+        if (hasOneOf(item.meta.access, access)) return true
+        else return false
+    } else return true
 }
 /**
  * @param {Array} list 通过路由列表得到菜单列表
  * @returns {Array}
  */
 export const getMenuByRouter = (list, access) => {
-  console.log(list)
-  let res = []
-  forEach(list, item => {
-    if (item.meta && !item.meta.hideInMenu) {
-      let obj = {
-        icon: (item.meta && item.meta.icon) || '',
-        name: item.name,
-        meta: item.meta
-      }
-      if (hasChild(item) && showThisMenuEle(item, access)) {
-        obj.children = getMenuByRouter(item.children, access)
-      }
-      if (showThisMenuEle(item, access)) res.push(obj)
-    }
-  })
-  return res
+    let res = []
+    forEach(list, item => {
+        if (item.meta && !item.meta.hideInMenu) {
+            let obj = {
+                icon: (item.meta && item.meta.icon) || '',
+                name: item.name,
+                meta: item.meta
+            }
+            if (hasChild(item) && showThisMenuEle(item, access)) {
+                obj.children = getMenuByRouter(item.children, access)
+            }
+            if (showThisMenuEle(item, access)) res.push(obj)
+        }
+    })
+    return res
 }
 
 /**
@@ -53,21 +52,21 @@ export const getMenuByRouter = (list, access) => {
  * @returns {Array}
  */
 export const getBreadCrumbList = (routeMetched) => {
-  let res = routeMetched.map(item => {
-    let obj = {
-      icon: (item.meta && item.meta.icon) || '',
-      name: item.name,
-      meta: item.meta
-    }
-    return obj
-  })
-  res = res.filter(item => {
-    return !item.meta.hideInMenu
-  })
-  return [{
-    name: 'home',
-    to: '/home'
-  }, ...res]
+    let res = routeMetched.map(item => {
+        let obj = {
+            icon: (item.meta && item.meta.icon) || '',
+            name: item.name,
+            meta: item.meta
+        }
+        return obj
+    })
+    res = res.filter(item => {
+        return !item.meta.hideInMenu
+    })
+    return [{
+        name: 'home',
+        to: '/home'
+    }, ...res]
 }
 
 export const showTitle = (item, vm) => vm.$config.useI18n ? vm.$t(item.name) : ((item.meta && item.meta.title) || item.name)
@@ -76,14 +75,14 @@ export const showTitle = (item, vm) => vm.$config.useI18n ? vm.$t(item.name) : (
  * @description 本地存储和获取标签导航列表
  */
 export const setTagNavListInLocalstorage = list => {
-  localStorage.tagNaveList = JSON.stringify(list)
+    localStorage.tagNaveList = JSON.stringify(list)
 }
 /**
  * @returns {Array} 其中的每个元素只包含路由原信息中的name, path, meta三项
  */
 export const getTagNavListFromLocalstorage = () => {
-  const list = localStorage.tagNaveList
-  return list ? JSON.parse(list) : []
+    const list = localStorage.tagNaveList
+    return list ? JSON.parse(list) : []
 }
 
 /**
@@ -91,19 +90,19 @@ export const getTagNavListFromLocalstorage = () => {
  * @description 用于找到路由列表中name为home的对象
  */
 export const getHomeRoute = routers => {
-  let i = -1
-  let len = routers.length
-  let homeRoute = {}
-  while (++i < len) {
-    let item = routers[i]
-    if (item.children && item.children.length) {
-      let res = getHomeRoute(item.children)
-      if (res.name) return res
-    } else {
-      if (item.name === 'home') homeRoute = item
+    let i = -1
+    let len = routers.length
+    let homeRoute = {}
+    while (++i < len) {
+        let item = routers[i]
+        if (item.children && item.children.length) {
+            let res = getHomeRoute(item.children)
+            if (res.name) return res
+        } else {
+            if (item.name === 'home') homeRoute = item
+        }
     }
-  }
-  return homeRoute
+    return homeRoute
 }
 
 /**
@@ -112,11 +111,11 @@ export const getHomeRoute = routers => {
  * @description 如果该newRoute已经存在则不再添加
  */
 export const getNewTagList = (list, newRoute) => {
-  const { name, path, meta } = newRoute
-  let newList = [...list]
-  if (newList.findIndex(item => item.name === name) >= 0) return newList
-  else newList.push({ name, path, meta })
-  return newList
+    const {name, path, meta} = newRoute
+    let newList = [...list]
+    if (newList.findIndex(item => item.name === name) >= 0) return newList
+    else newList.push({name, path, meta})
+    return newList
 }
 
 /**
@@ -124,10 +123,10 @@ export const getNewTagList = (list, newRoute) => {
  * @description 这里只是为了演示，实际应该将锁定状态的设置和获取用接口来实现
  */
 export const setLockStatus = (status) => {
-  localStorage.isLocked = status
+    localStorage.isLocked = status
 }
 export const getLockStatus = () => {
-  return parseInt(localStorage.isLocked)
+    return parseInt(localStorage.isLocked)
 }
 
 /**
@@ -135,8 +134,8 @@ export const getLockStatus = () => {
  * @param {*} route 路由列表
  */
 const hasAccess = (access, route) => {
-  if (route.meta && route.meta.access) return hasOneOf(access, route.meta.access)
-  else return true
+    if (route.meta && route.meta.access) return hasOneOf(access, route.meta.access)
+    else return true
 }
 
 /**
@@ -146,23 +145,23 @@ const hasAccess = (access, route) => {
  * @description 用户是否可跳转到该页
  */
 export const canTurnTo = (name, access, routes) => {
-  const getHasAccessRouteNames = (list) => {
-    let res = []
-    list.forEach(item => {
-      if (item.children && item.children.length) {
-        res = [].concat(res, getHasAccessRouteNames(item.children))
-      } else {
-        if (item.meta && item.meta.access) {
-          if (hasAccess(access, item)) res.push(item.name)
-        } else {
-          res.push(item.name)
-        }
-      }
-    })
-    return res
-  }
-  const canTurnToNames = getHasAccessRouteNames(routes)
-  return canTurnToNames.indexOf(name) > -1
+    const getHasAccessRouteNames = (list) => {
+        let res = []
+        list.forEach(item => {
+            if (item.children && item.children.length) {
+                res = [].concat(res, getHasAccessRouteNames(item.children))
+            } else {
+                if (item.meta && item.meta.access) {
+                    if (hasAccess(access, item)) res.push(item.name)
+                } else {
+                    res.push(item.name)
+                }
+            }
+        })
+        return res
+    }
+    const canTurnToNames = getHasAccessRouteNames(routes)
+    return canTurnToNames.indexOf(name) > -1
 }
 
 /**
@@ -170,13 +169,13 @@ export const canTurnTo = (name, access, routes) => {
  * @description 从URL中解析参数
  */
 export const getParams = url => {
-  const keyValueArr = url.split('?')[1].split('&')
-  let paramObj = {}
-  keyValueArr.forEach(item => {
-    const keyValue = item.split('=')
-    paramObj[keyValue[0]] = keyValue[1]
-  })
-  return paramObj
+    const keyValueArr = url.split('?')[1].split('&')
+    let paramObj = {}
+    keyValueArr.forEach(item => {
+        const keyValue = item.split('=')
+        paramObj[keyValue[0]] = keyValue[1]
+    })
+    return paramObj
 }
 
 /**
@@ -184,12 +183,12 @@ export const getParams = url => {
  * @param {String} name 当前关闭的标签的name
  */
 export const getNextName = (list, name) => {
-  let res = ''
-  if (list.length === 2) {
-    res = 'home'
-  } else {
-    if (list.findIndex(item => item.name === name) === list.length - 1) res = list[list.length - 2].name
-    else res = list[list.findIndex(item => item.name === name) + 1].name
-  }
-  return res
+    let res = ''
+    if (list.length === 2) {
+        res = 'home'
+    } else {
+        if (list.findIndex(item => item.name === name) === list.length - 1) res = list[list.length - 2].name
+        else res = list[list.findIndex(item => item.name === name) + 1].name
+    }
+    return res
 }
